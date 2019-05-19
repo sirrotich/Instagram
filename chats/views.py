a408b29b7b6977471aff2d7abe1c0fada8fa10b5
sirrotich <rotichtitus12@gmail.com>
2019-05-19 21:09:50 +0300

    create view functions for home and signup

--- chats/views.py
+++ chats/views.py
@@ -3,5 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 from django.http  import HttpResponse
 
-def index(request):
-    return HttpResponse('Instagram')
+def home(request):
+    images = Image.get_all_images()
+    
+    return render(request, 'index.html', {'images':images})
+
+def signup(request):
+    if request.user.is_authenticated():
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            form = SignupForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.is_active = False
+                user.save()
+                current_site = get_current_site(request)
+                to_email = form.cleaned_data.get('email')
+                send_activation_email(user, current_site, to_email)
+                return HttpResponse('Confirm your email address to complete registration')
+        else:
+            form = SignupForm()
+            return render(request, 'registration/signup.html',{'form':form})
