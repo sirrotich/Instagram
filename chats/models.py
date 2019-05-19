4075ef9f27538af8ce5c02e33dffdf33b2a6b228
sirrotich <rotichtitus12@gmail.com>
2019-05-19 21:13:08 +0300

    model class for user profile

--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    # profile_photo = models.ImageField(upload_to = 'profilepics/', default='Image')
+    prof_pic = ImageField(blank=True, manual_crop='800x800')
+    bio = HTMLField()
+    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
+
+    def save_profile(self):
+        self.save()
+    
+    @classmethod
+    def search_profile(cls, name):
+        profile = Profile.objects.filter(user__username__icontains = name)
+        return profile
+    
+    @classmethod
+    def get_by_id(cls, id):
+        profile = Profile.objects.get(user = id)
+        return profile
+
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user = id).first()
+        return profile
--- chats/urls.py
+++ chats/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
 
 urlpatterns=[
-    url('^$',views.index,name = 'index'),
+     url(r'^home/$', views.home, name='home'),
+    url(r'^$',views.signup, name='signup'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
+    url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
 ]
