df2acf0a2f46cf1ff49bc347154f5bf396163e69
sirrotich <rotichtitus12@gmail.com>
2019-05-19 21:35:35 +0300

    edit settings and project url file

--- instagram/settings.py
+++ instagram/settings.py
@@ -31,7 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'chats',
+    'pyuploadcare.dj',
+    'chats.apps.GramConfig',
+    'tinymce',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
