6cf9f22930d0b3a238826e84db196f95776e0ec7
sirrotich <rotichtitus12@gmail.com>
2019-05-22 16:23:10 +0300

    image field form

--- chats/forms.py
+++ chats/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Image, Profile, Comments
-
 from pyuploadcare.dj.forms import FileWidget
 from pyuploadcare.dj.models import ImageField
 
