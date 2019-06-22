c717ce3829d35402725453781a5f7212e600ec2a
sirrotich <rotichtitus12@gmail.com>
2019-06-22 20:37:32 +0300

    search posts functionality

--- chats/urls.py
+++ chats/urls.py
@@ -3,7 +3,6 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns=[
     url(r'^home/$', views.home, name='home'),
     url(r'^$',views.signup, name='signup'),
